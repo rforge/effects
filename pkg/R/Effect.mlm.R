ea751dcc0dab8724d98e2c17b72eeed00522f694
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-06 05:01:56 +0000

    remove roxygen markup from Effect.mlm.R. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/effects@152 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Effect.mlm.R
+++ pkg/R/Effect.mlm.R
@@ -1,20 +1,4 @@
-#' Calculate Effects for term(s) in a Multivariate Linear Model 
-
-#' This function calculates effects for some or all of the response
-#' variables for a term in a multivariate linear model.
-#' It uses \code{\link[stats]{update}} to evaluate the linear model
-#' for each response variable.
-
-#' @param focal.predictors the name(s) of predictor variables in the \code{mod} for which
-#'        to calculate effects
-#' @param mod a \code{mlm} object
-#' @param response name(s) or indices of one or more response variable(s).  The default is to use
-#'        all responses
-#' @return For one response, an \code{eff} object, otherwise a class \code{efflist} object, 
-#'          containing one \code{eff} object for each \code{response}
-
-#' @note This may require some minor adjustments to print and summary methods related to
-#'       labeling of output (response and term)
+#  Calculate Effects for term(s) in a Multivariate Linear Model 
 
 
 Effect.mlm <- function(focal.predictors, mod, response, ...) {
