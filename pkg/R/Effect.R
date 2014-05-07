8be5d167d963ac70d8faa9a5997a6583c8034ed9
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-07 14:12:52 +0000

    fix to Effect.gls(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/effects@167 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Effect.R
+++ pkg/R/Effect.R
@@ -10,6 +10,7 @@
 # 2013-11-06: fixed bug in Effect.multinom() in construction of effect object
 #             when there is only one focal predictor; caused as.data.frame.effpoly() to fail
 # 2014-03-13: modified Effect.lm() to compute partial residuals. J. Fox
+# 2014-05-06: fixed bug in Effect.gls() when cor or var structure depends on variables in the data set. J. Fox
 
 
 Effect <- function(focal.predictors, mod, ...){
@@ -275,71 +276,139 @@ Effect.lme <- function(focal.predictors, mod, ...) {
     result
 }
 
+# Effect.gls <- function (focal.predictors, mod, xlevels = list(), default.levels = NULL, given.values,
+#     se = TRUE, confidence.level = 0.95, 
+#     transformation = NULL, 
+#     typical = mean, ...){
+#     if (missing(given.values)) 
+#         given.values <- NULL
+#     else if (!all(which <- names(given.values) %in% names(coef(mod)))) 
+#         stop("given.values (", names(given.values[!which]), ") not in the model")
+#     formula.rhs <- formula(mod)[[3]]
+#     mod.lm <- lm(as.formula(mod$call$model), data=eval(mod$call$data))
+#     model.components <- Analyze.model(focal.predictors, mod.lm, xlevels, default.levels, formula.rhs)
+#     excluded.predictors <- model.components$excluded.predictors
+#     predict.data <- model.components$predict.data
+#     factor.levels <- model.components$factor.levels
+#     factor.cols <- model.components$factor.cols
+#     n.focal <- model.components$n.focal
+#     x <- model.components$x
+#     X.mod <- model.components$X.mod
+#     cnames <- model.components$cnames
+#     X <- model.components$X
+#     formula.rhs <- formula(mod)[c(1, 3)]
+#     nrow.X <- nrow(X)
+#     mf <- model.frame(formula.rhs, data=rbind(X[,names(predict.data),drop=FALSE], predict.data), 
+#         xlev=factor.levels)
+#     mod.matrix.all <- model.matrix(formula.rhs, data=mf, contrasts.arg=mod$contrasts)
+#     mod.matrix <- mod.matrix.all[-(1:nrow.X),]
+#     mod.matrix <- Fixup.model.matrix(mod.lm, mod.matrix, model.matrix(mod.lm), 
+#         X.mod, factor.cols, cnames, focal.predictors, excluded.predictors, typical, given.values)
+#     fit.1 <- na.omit(predict(mod))
+#     mod.2 <- lm.fit(mod.matrix.all[1:nrow.X,], fit.1)
+#     class(mod.2) <- "lm"
+#     #     assign(".y", na.omit(model.response.gls(mod)), envir=.GlobalEnv)
+#     #     assign(".X", na.omit(mod.matrix.all[1:nrow.X,]), envir=.GlobalEnv)
+#     .Data <- list(.y=na.omit(model.response.gls(mod)), .X=na.omit(mod.matrix.all[1:nrow.X,]))
+#     mod.3 <- update(mod, .y ~ .X - 1, data=.Data)
+#     #    remove(".X", ".y", envir=.GlobalEnv)
+#     discrepancy <- 100*mean(abs(fitted(mod.2)- fit.1)/(1e-10 + mean(abs(fit.1))))
+#     if (discrepancy > 1e-3) warning(paste("There is a discrepancy of", round(discrepancy, 3),
+#         "percent \n     in the 'safe' predictions used to generate effect", paste(focal.predictors, collapse="*")))
+#     effect <- mod.matrix %*% mod$coefficients
+#     result <- list(term = paste(focal.predictors, collapse="*"), 
+#         formula = formula(mod), response = response.name(mod), 
+#         variables = x, fit = effect, x = predict.data[, 1:n.focal, drop=FALSE], model.matrix = mod.matrix, data = X, 
+#         discrepancy = discrepancy, offset=0)
+#     if (se){
+#         df.res <- mod$dims[["N"]] - mod$dims[["p"]]
+#         z <- qt(1 - (1 - confidence.level)/2, df=df.res)
+#         mod.2$terms <- terms(mod)
+#         V <- vcov(mod.3)
+#         vcov <- mod.matrix %*% V %*% t(mod.matrix)
+#         rownames(vcov) <- colnames(vcov) <- NULL
+#         var <- diag(vcov)
+#         result$vcov <- vcov
+#         result$se <- sqrt(var)        
+#         result$lower <- effect - z*result$se
+#         result$upper <- effect + z*result$se
+#         result$confidence.level <- confidence.level
+#     }
+#     if (is.null(transformation$link) && is.null(transformation$inverse)){
+#         transformation$link <- I
+#         transformation$inverse <- I
+#     }
+#     result$transformation <- transformation
+#     class(result) <- "eff"
+#     result
+# }
+
 Effect.gls <- function (focal.predictors, mod, xlevels = list(), default.levels = NULL, given.values,
-    se = TRUE, confidence.level = 0.95, 
-    transformation = NULL, 
-    typical = mean, ...){
-    if (missing(given.values)) 
-        given.values <- NULL
-    else if (!all(which <- names(given.values) %in% names(coef(mod)))) 
-        stop("given.values (", names(given.values[!which]), ") not in the model")
-    formula.rhs <- formula(mod)[[3]]
-    mod.lm <- lm(as.formula(mod$call$model), data=eval(mod$call$data))
-    model.components <- Analyze.model(focal.predictors, mod.lm, xlevels, default.levels, formula.rhs)
-    excluded.predictors <- model.components$excluded.predictors
-    predict.data <- model.components$predict.data
-    factor.levels <- model.components$factor.levels
-    factor.cols <- model.components$factor.cols
-    n.focal <- model.components$n.focal
-    x <- model.components$x
-    X.mod <- model.components$X.mod
-    cnames <- model.components$cnames
-    X <- model.components$X
-    formula.rhs <- formula(mod)[c(1, 3)]
-    nrow.X <- nrow(X)
-    mf <- model.frame(formula.rhs, data=rbind(X[,names(predict.data),drop=FALSE], predict.data), 
-        xlev=factor.levels)
-    mod.matrix.all <- model.matrix(formula.rhs, data=mf, contrasts.arg=mod$contrasts)
-    mod.matrix <- mod.matrix.all[-(1:nrow.X),]
-    mod.matrix <- Fixup.model.matrix(mod.lm, mod.matrix, model.matrix(mod.lm), 
-        X.mod, factor.cols, cnames, focal.predictors, excluded.predictors, typical, given.values)
-    fit.1 <- na.omit(predict(mod))
-    mod.2 <- lm.fit(mod.matrix.all[1:nrow.X,], fit.1)
-    class(mod.2) <- "lm"
-    #     assign(".y", na.omit(model.response.gls(mod)), envir=.GlobalEnv)
-    #     assign(".X", na.omit(mod.matrix.all[1:nrow.X,]), envir=.GlobalEnv)
-    .Data <- list(.y=na.omit(model.response.gls(mod)), .X=na.omit(mod.matrix.all[1:nrow.X,]))
-    mod.3 <- update(mod, .y ~ .X - 1, data=.Data)
-    #    remove(".X", ".y", envir=.GlobalEnv)
-    discrepancy <- 100*mean(abs(fitted(mod.2)- fit.1)/(1e-10 + mean(abs(fit.1))))
-    if (discrepancy > 1e-3) warning(paste("There is a discrepancy of", round(discrepancy, 3),
-        "percent \n     in the 'safe' predictions used to generate effect", paste(focal.predictors, collapse="*")))
-    effect <- mod.matrix %*% mod$coefficients
-    result <- list(term = paste(focal.predictors, collapse="*"), 
-        formula = formula(mod), response = response.name(mod), 
-        variables = x, fit = effect, x = predict.data[, 1:n.focal, drop=FALSE], model.matrix = mod.matrix, data = X, 
-        discrepancy = discrepancy, offset=0)
-    if (se){
-        df.res <- mod$dims[["N"]] - mod$dims[["p"]]
-        z <- qt(1 - (1 - confidence.level)/2, df=df.res)
-        mod.2$terms <- terms(mod)
-        V <- vcov(mod.3)
-        vcov <- mod.matrix %*% V %*% t(mod.matrix)
-        rownames(vcov) <- colnames(vcov) <- NULL
-        var <- diag(vcov)
-        result$vcov <- vcov
-        result$se <- sqrt(var)        
-        result$lower <- effect - z*result$se
-        result$upper <- effect + z*result$se
-        result$confidence.level <- confidence.level
-    }
-    if (is.null(transformation$link) && is.null(transformation$inverse)){
-        transformation$link <- I
-        transformation$inverse <- I
-    }
-    result$transformation <- transformation
-    class(result) <- "eff"
-    result
+                        se = TRUE, confidence.level = 0.95, 
+                        transformation = NULL, 
+                        typical = mean, ...){
+  if (missing(given.values)) 
+    given.values <- NULL
+  else if (!all(which <- names(given.values) %in% names(coef(mod)))) 
+    stop("given.values (", names(given.values[!which]), ") not in the model")
+  formula.rhs <- formula(mod)[[3]]
+  .data <- eval(mod$call$data)
+  mod.lm <- lm(as.formula(mod$call$model), data=.data, na.action=na.exclude)
+  model.components <- Analyze.model(focal.predictors, mod.lm, xlevels, default.levels, formula.rhs)
+  excluded.predictors <- model.components$excluded.predictors
+  predict.data <- model.components$predict.data
+  factor.levels <- model.components$factor.levels
+  factor.cols <- model.components$factor.cols
+  n.focal <- model.components$n.focal
+  x <- model.components$x
+  X.mod <- model.components$X.mod
+  cnames <- model.components$cnames
+  X <- model.components$X
+  formula.rhs <- formula(mod)[c(1, 3)]
+  nrow.X <- nrow(X)
+  mf <- model.frame(formula.rhs, data=rbind(X[,names(predict.data),drop=FALSE], predict.data), 
+                    xlev=factor.levels)
+  mod.matrix.all <- model.matrix(formula.rhs, data=mf, contrasts.arg=mod$contrasts)
+  mod.matrix <- mod.matrix.all[-(1:nrow.X),]
+  mod.matrix <- Fixup.model.matrix(mod.lm, mod.matrix, model.matrix(mod.lm), 
+                                   X.mod, factor.cols, cnames, focal.predictors, excluded.predictors, typical, given.values)
+  fit.1 <- na.omit(predict(mod))
+  mod.2 <- lm.fit(mod.matrix.all[1:nrow.X,], fit.1)
+  class(mod.2) <- "lm"
+  use <- !is.na(residuals(mod.lm))
+  .data <- .data[use, ]
+  .data$.y <- model.response.gls(mod)
+  .data$.X <- mod.matrix.all[1:nrow.X, ]
+  mod.3 <- update(mod, .y ~ .X - 1, data=.data)
+  discrepancy <- 100*mean(abs(fitted(mod.2)- fit.1)/(1e-10 + mean(abs(fit.1))))
+  if (discrepancy > 1e-3) warning(paste("There is a discrepancy of", round(discrepancy, 3),
+                                        "percent \n     in the 'safe' predictions used to generate effect", paste(focal.predictors, collapse="*")))
+  effect <- mod.matrix %*% mod$coefficients
+  result <- list(term = paste(focal.predictors, collapse="*"), 
+                 formula = formula(mod), response = response.name(mod), 
+                 variables = x, fit = effect, x = predict.data[, 1:n.focal, drop=FALSE], model.matrix = mod.matrix, data = X, 
+                 discrepancy = discrepancy, offset=0)
+  if (se){
+    df.res <- mod$dims[["N"]] - mod$dims[["p"]]
+    z <- qt(1 - (1 - confidence.level)/2, df=df.res)
+    mod.2$terms <- terms(mod)
+    V <- vcov(mod.3)
+    vcov <- mod.matrix %*% V %*% t(mod.matrix)
+    rownames(vcov) <- colnames(vcov) <- NULL
+    var <- diag(vcov)
+    result$vcov <- vcov
+    result$se <- sqrt(var)        
+    result$lower <- effect - z*result$se
+    result$upper <- effect + z*result$se
+    result$confidence.level <- confidence.level
+  }
+  if (is.null(transformation$link) && is.null(transformation$inverse)){
+    transformation$link <- I
+    transformation$inverse <- I
+  }
+  result$transformation <- transformation
+  class(result) <- "eff"
+  result
 }
 
 Effect.multinom <- function(focal.predictors, mod, 
